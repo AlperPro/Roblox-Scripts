4b59e14a32f0dddd3f378c47c5c55a4b6f12681a
Pro <124919877+AlperPro@users.noreply.github.com>
2025-06-26 01:36:23 +0300

    Update LloydHUBLoader.lua

--- LloydHUBLoader.lua
+++ LloydHUBLoader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 94647229517154 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/AlperPro/Roblox-Scripts/refs/heads/main/AzureLatch.lua')))()
-elseif game.PlaceId == 18668065416 then
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/AlperPro/Roblox-Scripts/refs/heads/main/BlueLockRivals.lua')))()
+else
+  game.Players.LocalPlayer:Kick("USE THIS SCRIPT ONLY IN AZURE LATCH!")
 end
