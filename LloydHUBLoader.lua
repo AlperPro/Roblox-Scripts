7a2c62927c9d68e641ce85a85f46732e43138a85
Pro <124919877+AlperPro@users.noreply.github.com>
2025-07-13 05:02:33 +0300

    Update LloydHUBLoader.lua

--- LloydHUBLoader.lua
+++ LloydHUBLoader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 94647229517154 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/AlperPro/Roblox-Scripts/refs/heads/main/AzureLatch.lua')))()
+elseif game.PlaceId == 137628283909891 then
+  loadstring(game:HttpGet(('https://raw.githubusercontent.com/AlperPro/Roblox-Scripts/refs/heads/main/Goalbound.lua')))()
 else
-  game.Players.LocalPlayer:Kick("USE THIS SCRIPT ONLY IN AZURE LATCH!")
+  game.Players.LocalPlayer:Kick("USE THIS SCRIPT ONLY IN AZURE LATCH OR GOALBOUND!")
 end
