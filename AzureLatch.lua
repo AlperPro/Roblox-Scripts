49667e285ba1e2e62b4cc5cd54f57148be41f923
Pro <124919877+AlperPro@users.noreply.github.com>
2025-08-07 13:09:26 +0300

    Changed method and fixed executor support for swift etc.

--- AzureLatch.lua
+++ AzureLatch.lua
@@ -1,2 +1,2 @@
 setclipboard("discord.gg/NcygVDSb3J")
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/2f59a2c96b20a388"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/9b34ff6bdb1055ae"))()
