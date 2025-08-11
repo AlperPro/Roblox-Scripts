eb32293c041850cad93039db2c248e9136c6247a
Pro <124919877+AlperPro@users.noreply.github.com>
2025-08-11 17:35:30 +0300

    Update AzureLatch.lua

--- AzureLatch.lua
+++ AzureLatch.lua
@@ -1,2 +1,2 @@
 setclipboard("discord.gg/NcygVDSb3J")
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/9b34ff6bdb1055ae"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/421328c6f0a8ca63"))()
